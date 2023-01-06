5a74e27de221ab1fa6b8e7993787a6cb20370ee3
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-01-06 20:06:20 +0000

    added function for password options with prompts and confirms. created vars to hold collected user choices, created if statements to re run function if invalid number submitted. Created random generator with Math.random to return random password with chosen criteria, and displayed this to the browser

---
+++ starter/script.js
@@ -0,0 +1,158 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+var passwordChoices = []
+
+
+// Function to prompt user for password options
+//prompt each and validate, store data
+
+function getPasswordOptions() {
+  var passwordLength = prompt('How many characters would you like? (Between 10-64)')
+   if (passwordLength  < 10 || passwordLength  > 64) {
+    alert('Please pick a number between 10-64.')
+    getPasswordOptions()
+} else {
+  var lower = confirm('Would you like to use lower case characters?')
+  var upper = confirm('Would you like to use upper case characters?')
+  var special = confirm('Would you like to use special characters?')
+  var numbers = confirm('Would you like to use numbers?')
+  if (lower === true) {
+    passwordChoices.push(...lowerCasedCharacters)
+  }
+  if (upper === true) {
+    passwordChoices.push(...upperCasedCharacters)
+  }
+  if (special === true) {
+    passwordChoices.push(...specialCharacters)
+  }
+  if (numbers === true) {
+    passwordChoices.push(...numericCharacters)
+  }
+  console.log(passwordChoices)
+
+  var collectedPasswordChoices = []
+
+  for ( var i = 0; i < passwordLength; i++) {
+    collectedPasswordChoices.push(passwordChoices[(Math.floor(Math.random() * passwordChoices.length))])
+  }
+  console.log(collectedPasswordChoices)
+  return collectedPasswordChoices.join('')
+}
+}
+
+//getPasswordOptions()
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = getPasswordOptions();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+
+
+/* Function for getting a random element from an array
+function getRandom(arr) {
+//Math.random()
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+} //value = generatePassword(length. value, characters); }); */
